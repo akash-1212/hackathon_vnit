b42e710c3774656d1c580d673b0fb3557273607e
root <aakashbarapatre@gmail.com>
2016-04-14 13:40:53 +0530

    views started

--- hura/assignment/models.py
+++ hura/assignment/models.py
@@ -1,5 +1,48 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.admin import User
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    user=models.OneToOneField(User)
+    name=models.CharField(max_length=50)
+    branch=models.CharField(max_length=3)
+    email=models.EmailField()
+    section=models.CharField(max_length=1)
+    # courses=models.CharField(max_length=60)
+
+class Professor(models.Model):
+    user=models.OneToOneField(User)
+    name=models.CharField(max_length=50,primary_key=True)
+    # courses=models.CharField(max_length=50)
+    email=models.EmailField()
+
+class Course(models.Model):
+    course_id=models.CharField(max_length=6,primary_key=True)
+    course_name=models.CharField(max_length=40)
+
+class Prof_course(models.Model):
+    course=models.ForeignKey(Course)
+    prof=models.ForeignKey(Professor)
+
+class Stud_course(models.Model):
+    course=models.ForeignKey(Course)
+    stud=models.ForeignKey(Student)
+    reg_time=models.DateField()
+
+
+class Assignment(models.Model):
+    id=models.CharField(max_length=10,primary_key=True)
+    name=models.CharField(max_length=10)
+    course=models.ForeignKey(Course)
+    prof=models.ForeignKey(Professor)
+    time_uploaded=models.DateField(auto_now=True)
+
+class assign_section(models.Model):
+    assign=models.ForeignKey(Assignment)
+    deadline=models.DateField()
+    section=models.CharField(max_length=1)
+
+class stud_subm(models.Model):
+    stud=models.ForeignKey(Student)
+    assignment=models.ForeignKey(Assignment)
+    subm_time=models.DateField()
--- hura/assignment/views.py
+++ hura/assignment/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
+from django.template import RequestContext
+
+from assignment.models import Professor
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label=(u'username'))
+    password = forms.CharField(label=(u'password'), widget=forms.PasswordInput(render_value=False))
+
+def Login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                if Professor.objects.filter(user=user).exists():
+                    return HttpResponseRedirect('/prof_welcome')
+                else:
+                    return HttpResponseRedirect('/stud_welcome')
+            else:
+                errors = form._errors.setdefault("no_field", form.error_class())
+                errors.append("validation Error")
+                return render_to_response('login.html', {'form': form},
+                                          context_instance=RequestContext(request))
+        else:
+            return render_to_response('login.html', {'form': form}, context_instance=RequestContext(request))
+    else:
+        '''user is not submitting the form, show the login form'''
+        form = LoginForm()
+        context = {'form': form}
+        return render_to_response('login.html', context, context_instance=RequestContext(request))
+
+
+def StudWelcome(request):
+    if request.user.is_authenticated():
+
+    else :
+        HttpResponseRedirect('insti/login/')
